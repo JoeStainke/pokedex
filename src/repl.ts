78be9e576098471601918873b0fc98606970407e
Joe Stainke <joe.stainke@gdotv.com>
2025-08-06 10:21:54 +0100

    Initial commit - Pokedex CLI REPL

---
+++ src/repl.ts
@@ -0,0 +1,24 @@
+import { createInterface } from "node:readline";
+
+export function cleanInput(input: string): string[] {
+  return input.trim().toLowerCase().split(/\s+/); // regex for one or more whitespace characters
+}
+export function startREPL() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt(); // Call prompt on the interface instance
+
+  rl.on("line", (input: string) => {
+    const words = cleanInput(input);
+    if (!cleanInput(input).length) {
+      rl.prompt();
+    } else {
+      console.log(`Your command was: ${cleanInput(words[0])}`);
+      rl.prompt(); // Call prompt again to continue the loop
+    }
+  });
+}
